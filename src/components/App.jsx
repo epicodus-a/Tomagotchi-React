190f0e85d9a94035ab06717d019b7643d2ee5b06
Adriana C and Sean McDermott <sean@>
2018-07-03 13:50:38 -0700

    get radio value correctly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SimpleAppBar from './testmaterial';
+// import Food from './Food';
+import SimpleMediaCard from './Food';
+import HomePage from './HomePage';
 
-function App(){
-  return (
-    <div>
-      <Switch>
-        <Route path='/' component={SimpleAppBar} />
-      </Switch>
-    </div>
-  );
+// function App(){
+//   return (
+//     <div>
+//       <Switch>
+//         <Route path='/' component={SimpleAppBar} />
+//       </Switch>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      sleepLevel: 100,
+      foodLevel: 100,
+      playLevel: 100
+    }
+  }
+  render(){
+    return(
+      <div>
+       <Switch>
+         <Route path='/' exact component={HomePage} />
+        //  <Route path='/' exact component={SimpleAppBar} />
+
+       </Switch>
+     </div>
+    )
+  }
 }
 
 export default App;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,2 +1,75 @@
 import React from 'react';
-// import tapHouse from '../assets/img/tap-house.jpg';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import sleep from '../assets/img/tsleep.png';
+import play from '../assets/img/tplay.gif';
+import food from '../assets/img/tfood.gif';
+
+
+
+class HomePage extends React.Component{
+  // let _food = null;
+  // let _sleep = null;
+  // let _play = null;
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedOption: 'sleep'
+    }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+  }
+
+  handleOptionChange(changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value,
+    });
+  }
+
+  handleFormSubmit(formSubmitEvent) {
+    formSubmitEvent.preventDefault();
+    console.log('You have selected:', this.state.selectedOption);
+  }
+  render() {
+
+
+    return (
+      <div>
+        <div>
+          <img src={sleep} className='sleep'/>
+          <img src={play} className='play'/>
+          <img src={food} className='food'/>
+        </div>
+        <form onSubmit={this.handleFormSubmit}>
+        <div className="radio">
+          <label>
+            <input type="radio"
+                   value="sleep"
+                   checked={this.state.selectedOption === 'sleep'} onChange={this.handleOptionChange} />
+            Sleep
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio"
+                   value="play"
+                   checked={this.state.selectedOption === 'play'} onChange={this.handleOptionChange} />
+            Play
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio"
+                   value="food"
+                   checked={this.state.selectedOption === 'food'} onChange={this.handleOptionChange} />
+            Food
+          </label>
+        </div>
+        <button classname="button" type='submit'>Choose One</button>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
